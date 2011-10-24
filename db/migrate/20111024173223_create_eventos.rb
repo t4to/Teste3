de9d004b19505ec63382457d281d3e7328ef35b6
LRC <tato.rodrigues@gmail.com>
2011-10-24 16:12:21 -0200

    Models

---
+++ db/migrate/20111024173223_create_eventos.rb
@@ -0,0 +1,15 @@
+class CreateEventos < ActiveRecord::Migration
+  def self.up
+    create_table :eventos do |t|
+      t.string :nome
+      t.string :user
+      t.string :info
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :eventos
+  end
+end
